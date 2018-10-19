774aef6bc6bad761298d6377e8470259c80c8c34
Alex Lementuev <a.lementuev@gmail.com>
2018-10-19 14:07:03 -0700

    Bumped SDK version to 5.3.0

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("5.1.0")]
+[assembly: AssemblyVersion("5.3.0")]
 [assembly: MetaData("apptentive_sdk_distribution", Value = "Xamarin")]
 [assembly: MetaData("apptentive_sdk_distribution_version", Value = "5.1.0")]
 
