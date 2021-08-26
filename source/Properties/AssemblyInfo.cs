861c4b411ee7a5e083653beb6089198b1ac1fa8c
Twinkle Sharma <twinkle.sharma@apptentive.com>
2021-08-26 14:59:14 -0700

    updated assembly version

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("5.7.0")]
+[assembly: AssemblyVersion("5.7.1")]
 [assembly: MetaData("apptentive_sdk_distribution", Value = "Xamarin")]
 [assembly: MetaData("apptentive_sdk_distribution_version", Value = "5.7.1")]
 
